00cb14559c3e9cf34eee463088c4d0a74f5208d1
Dan Tran <autumnssuns@gmail.com>
2023-05-05 00:44:54 +1000

    Now caches movies

--- client/src/utils/fetchTransform.js
+++ client/src/utils/fetchTransform.js
@@ -1,4 +1,5 @@
 import { movies } from '../mocks/movie.search.js';
+import { IndexedDB } from './indexed.js';
 
 /**
  * Get data from the given endpoint, relative to the BASE_API_URL defined in the
@@ -17,28 +18,26 @@ export async function getEndpoint(
 
     // Fetch the data from the endpoint and return the transformed data.
     try {
-        // If the BASE_API_URL is 'local', then use the mock data.
-        let response;
-        if (BASE_API_URL === 'local'){
-            response = {
-                json: () => {
-                    return movies;
-                }
-            };
-        } else {
-            response = await fetch(`${BASE_API_URL}${endpoint}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-                signal: signal
-            });
+        // Get the data from the indexedDB if it exists.
+        const indexedData = await getDataFromIndexedDB(endpoint);
+        if (indexedData){
+            return transformer(indexedData);
         }
+        // Otherwise, fetch the data from the API and save it to the indexedDB.
+        const response = await fetch(`${BASE_API_URL}${endpoint}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            signal: signal
+        });
         const data = await response.json();
         if (data.error){
             throw new Error(data.message);
         }
+        // Save the data to the indexedDB.
+        await addDataToIndexedDB(endpoint, data);
         return transformer(data);
     }
     catch (error) {
@@ -56,21 +55,13 @@ export async function postEndpoint(endpoint, body){
     try {
         // If the BASE_API_URL is 'local', then use the mock data.
         let response;
-        if (BASE_API_URL === 'local'){
-            response = {
-                json: () => {
-                    return movies;
-                }
-            };
-        } else {
-            response = await fetch(`${BASE_API_URL}${endpoint}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(body)
-            });
-        }
+        response = await fetch(`${BASE_API_URL}${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
         const data = await response.json();
         if (data.error){
             throw new Error(data.message);
@@ -81,5 +72,50 @@ export async function postEndpoint(endpoint, body){
         console.log(error)
         // Throw the error so that the caller can handle it.
         throw error;
+    }
+}
+
+/**
+ * Get data from the indexedDB based on the endpoint.
+ * @param {*} endpoint The endpoint to fetch data from, e.g. '/movies/data/{imdbID}'.
+ * @returns {Promise} A promise that resolves when the data has been retrieved from the indexedDB.
+ */
+async function getDataFromIndexedDB(endpoint){
+    // Get the parts of the endpoint - id and base.
+    // e.g. '/movies/data/tt1234567' => id = 'tt1234567', base = '/movies/data'
+    const parts = endpoint.split('/');
+    const id = parts[parts.length - 1];
+    const base = parts.slice(0, parts.length - 1).join('/');
+    
+    switch (base){
+        case '/movies/data':
+            return await IndexedDB.Movies.read(id);
+        case '/people/':
+            return await IndexedDB.People.read(id);
+        default:
+            return null;
+    }
+}
+
+/**
+ * Add data to the indexedDB based on the endpoint.
+ * @param {*} endpoint The endpoint to fetch data from, e.g. '/movies/data/{imdbID}'.
+ * @param {*} data The data to add to the indexedDB.
+ * @returns {Promise} A promise that resolves when the data has been added to the indexedDB.
+ */
+async function addDataToIndexedDB(endpoint, data){
+    // Get the parts of the endpoint - id and base.
+    // e.g. '/movies/data/tt1234567' => id = 'tt1234567', base = '/movies/data'
+    const parts = endpoint.split('/');
+    const id = parts[parts.length - 1];
+    const base = parts.slice(0, parts.length - 1).join('/');
+    
+    switch (base){
+        case '/movies/data':
+            return await IndexedDB.Movies.create(id, data);
+        case '/people/':
+            return await IndexedDB.People.create(id, data);
+        default:
+            return null;
     }
 }
