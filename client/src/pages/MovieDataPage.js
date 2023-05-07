d7ef82951d8dc03807d44122825b0c1315acdf0b
Dan Tran <autumnssuns@gmail.com>
2023-05-08 00:19:23 +1000

    Added genres and countries to movie data page

--- client/src/pages/MovieDataPage.js
+++ client/src/pages/MovieDataPage.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Badge } from "reactstrap";
 import { AgGridReact } from "ag-grid-react";
 import { ScoreCircle } from "../components/ScoreCircle/ScoreCircle";
+import { getCode } from "../utils/countries";
+import Flag from "react-world-flags";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import "./MovieDataPage.css";
@@ -16,6 +18,15 @@ function formatCurrency(number) {
     currency: "USD",
     maximumFractionDigits: 0,
   }).format(number);
+}
+
+function formatRuntime(runtime) {
+  if (typeof(runtime) !== "number" || isNaN(runtime)) {
+    return "Runtime Unknown";
+  }
+  const hours = Math.floor(runtime / 60);
+  const minutes = runtime % 60;
+  return `${hours}h ${minutes}m`;
 }
 
 function Rating({rating}) {
@@ -48,6 +59,38 @@ function Rating({rating}) {
       </span>
     </div>
   );
+}
+
+function Genres({genres}) {
+  return (
+    <div className="genres-container">
+      {genres.map((genre) => {
+        return (
+          <div key={genre} className="genre">
+            {genre}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Countries({countries}){
+  const countryComponents = countries.map((countryName) => {
+    const country = getCode(countryName.trim());
+    console.log(country);
+    return (
+      <div key={countryName} className="country">
+        <Flag key={country} code={country} height="16" />
+        {countryName}
+      </div>
+    );
+  });
+  return (
+    <div className="countries-container">
+      {countryComponents.reduce((prev, curr) => [prev, <div className="vertical-separator"/>, curr])}
+    </div>
+  )
 }
 
 export default function MovieDataPage() {
@@ -111,50 +154,60 @@ export default function MovieDataPage() {
   }
 
   return (
-    <div className="movie-data-page">
-      <div className="movie-data-container">
-        <img src={movie.poster} alt={movie.title} />
-        <div className="details-container">
-          <div className="info-container">
-            <h1>{movie.title}</h1>
-            <p>Released in: {movie.year}</p>
-            <p>Runtime: {movie.runtime}</p>
-            <p>
-              Genres:{" "}
-              {movie.genres.map((genre) => {
-                return <Badge bg="secondary">{genre} </Badge>;
-              })}
-            </p>
-            <p>Country: {movie.country}</p>
-            <p>Box Office: {formatCurrency(movie.boxoffice)}</p>
-          </div>
-          <div className="plot-container">
-            <p>{movie.plot}</p>
-          </div>
-          <div className="ratings-container">
-            <div style={{
-              display: "flex",
-              flexWrap: "wrap"
-            }}>
-              {movie.ratings.filter((rating) => {
-                return typeof(rating.value) === "number";
-              }).map((rating) => {
-                return (
-                  <Rating rating={rating} key={rating.source}></Rating>
-                );
-              })}
+    <div className="movie-data-page" style={{
+      backgroundImage: `url(${movie.poster})`,
+    }}>
+      <div>
+        <div className="movie-data-container">
+          <img src={movie.poster} alt={movie.title} />
+          <div className="details-container">
+            <div className="info-container">
+              <h1>{movie.title}</h1>
+              <div className="general-info-container">
+                <div className="time-info-container">
+                  <div>{movie.year}</div>
+                  <div className="vertical-separator"></div>
+                  <div>{formatRuntime(movie.runtime)}</div>
+                </div>
+                <div className="vertical-separator"></div>
+                <Genres genres={movie.genres}></Genres>
+              </div>
+              <div className="plot-container">
+                <p>{movie.plot}</p>
+              </div>
+            </div>
+            <div className="production-container">
+              {/* <div>{movie.country} <Flag code={getCode(movie.country)} height="50%"/></div> */}
+              <Countries countries={movie.country.split(",")}></Countries>
+            </div>
+            <div><strong>Box Office</strong>: {formatCurrency(movie.boxoffice)}</div>
+            <div className="ratings-container">
+              <div style={{
+                display: "flex",
+                flexWrap: "wrap"
+              }}>
+                {movie.ratings.filter((rating) => {
+                  return typeof(rating.value) === "number";
+                }).map((rating) => {
+                  return (
+                    <Rating rating={rating} key={rating.source}></Rating>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="principals-container ag-theme-alpine-dark">
-        <h3>Principals</h3>
-        <AgGridReact
-          defaultColDef={defaultColsDef}
-          rowData={movie.principals}
-          columnDefs={columns}
-          domLayout="autoWidth"
-        />
+        <div className="principals-box">
+          <h3>Principals</h3>
+          <div className="principals-container ag-theme-alpine-dark">
+            <AgGridReact
+              defaultColDef={defaultColsDef}
+              rowData={movie.principals}
+              columnDefs={columns}
+              domLayout="autoWidth"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
