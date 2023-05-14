152cdc91aae8e24c018777a17d3edb749549200d
Dan Tran <autumnssuns@gmail.com>
2023-05-14 19:02:20 +1000

    Modified sample playwright test

--- client/e2e/example.spec.js
+++ client/e2e/example.spec.js
@@ -5,5 +5,5 @@ test('has title', async ({ page }) => {
   await page.goto('localhost:3000');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/React App/);
+  await expect(page).toHaveTitle(/CineLib/);
 });
