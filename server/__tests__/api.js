2d2c6fcc379b21baa19df51a6b35cbb822f2f83c
Dan Tran <autumnssuns@gmail.com>
2023-06-02 14:23:23 +1000

    Added self-signed certificate

--- server/__tests__/api.js
+++ server/__tests__/api.js
@@ -1,3 +1,3 @@
 //If you are serving your server on any port other than 3000, change the port here, or alternatively change the url to approriate
-const REMOTE_API_URL = `http://localhost:3000`;
+const REMOTE_API_URL = `https://localhost:3000`;
 module.exports = REMOTE_API_URL;
